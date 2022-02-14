05b4848c89d5d443dd25549aa011e6970995620f
George-Lab <52816959+George-Lab@users.noreply.github.com>
2022-02-14 20:44:28 +0500

    Add files via upload

---
+++ ph.js
@@ -0,0 +1,5 @@
+var element = document.getElementById("age-verification-container");
+element.parentNode.removeChild(element);
+
+element = document.getElementById("age-verification-wrapper");
+element.parentNode.removeChild(element);
